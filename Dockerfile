982a1efd82c05d5b2989d8914dae166b322f5883
Raphael <raphaelmonticello@hotmail.com>
2024-02-25 00:48:13 -0800

    Validacoes resolvidas

--- Dockerfile
+++ Dockerfile
@@ -1,32 +1,45 @@
-# Use an official Golang Alpine image to create a build artifact.
+# First stage: build the application
 FROM golang:1.22-alpine as builder
 
-# Install git, required for fetching Go modules.
-# Also, ca-certificates is a common requirement for applications making HTTPS requests.
-RUN apk add --no-cache git ca-certificates
+# Install build dependencies
+RUN apk add --no-cache git
 
-# Set the Current Working Directory inside the container
+# Set the working directory inside the container
 WORKDIR /app
 
-# Copy everything from the current directory to the PWD (Present Working Directory) inside the container
+# Copy go mod and sum files
+COPY go.mod go.sum ./
+
+# Download all dependencies
+RUN go mod download
+
+# Copy the source code into the container
 COPY . .
 
-# Fetch dependencies.
-# Using go mod with Go 1.11 modules.
-RUN go mod tidy
+# Build the application
+RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o myapp .
 
-# Build the Go app as a static binary.
-RUN CGO_ENABLED=0 go build -o /rinha
+# Second stage: setup the runtime environment
+FROM alpine:3.13
 
-# Start from a fresh Alpine image to create a smaller final image
-FROM alpine:3.18.3
+# Install bash for wait-for-it.sh and timezone data
+RUN apk add --no-cache bash tzdata
 
-# Import the user and group files from the builder.
-COPY --from=builder /etc/passwd /etc/passwd
-COPY --from=builder /etc/group /etc/group
+# Set the timezone (optional)
+RUN cp /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime \
+    && echo "America/Sao_Paulo" > /etc/timezone
 
-# Copy the static executable.
-COPY --from=builder /rinha /rinha
+# Create app directory
+WORKDIR /app
 
-# Run the binary.
-CMD ["/rinha"]
+# Copy the binary from the builder stage
+COPY --from=builder /app/myapp .
+
+# Copy wait-for-it.sh into the image
+COPY wait-for-it.sh wait-for-it.sh
+
+# Make wait-for-it.sh executable
+RUN chmod +x wait-for-it.sh
+
+# Command to run the application using wait-for-it.sh
+CMD ["./wait-for-it.sh", "db:5432", "--timeout=30", "--", "./myapp"]
--- server.go
+++ server.go
@@ -1,41 +1,93 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strconv"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
-type Transaction struct {
-	Valor       int       `json:"valor"`
-	Tipo        string    `json:"tipo"`
-	Descricao   string    `json:"descricao"`
-	RealizadaEm time.Time `json:"realizada_em"`
-}
-
 type Cliente struct {
-	ID         int
-	Limite     int `json:"limite"`
-	Saldo      int `json:"saldo"`
-	Transacoes []Transaction
+	ID         uint        `gorm:"primaryKey"`
+	Limite     int         `json:"limite"`
+	Saldo      int         `json:"saldo"`
+	Transacoes []Transacao `gorm:"foreignKey:ClienteID"`
 }
 
-var clientes = map[int]*Cliente{
-	1: {ID: 1, Limite: 100000, Saldo: 0, Transacoes: []Transaction{}},
-	2: {ID: 2, Limite: 80000, Saldo: 0, Transacoes: []Transaction{}},
-	3: {ID: 3, Limite: 1000000, Saldo: 0, Transacoes: []Transaction{}},
-	4: {ID: 4, Limite: 10000000, Saldo: 0, Transacoes: []Transaction{}},
-	5: {ID: 5, Limite: 500000, Saldo: 0, Transacoes: []Transaction{}},
+type Transacao struct {
+	ID        uint      `gorm:"primaryKey" json:"-"`
+	ClienteID uint      `json:"-"`
+	Valor     int       `json:"valor"`
+	Tipo      string    `json:"tipo"`
+	Descricao string    `json:"descricao"`
+	CreatedAt time.Time `json:"realizada_em"`
+}
+
+var db *gorm.DB
+var err error
+
+func initDatabase() {
+	// Retrieve database connection details from environment variables
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+	sslmode := "disable"            // Usually, for local development, SSL mode is disabled. Adjust as necessary.
+	timeZone := "America/Sao_Paulo" // Make sure to use the correct timezone
+
+	// Construct the DSN (Data Source Name)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s", host, user, password, dbname, port, sslmode, timeZone)
+
+	// Open the database connection
+	var err error
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&Cliente{}, &Transacao{})
+
+	// Seed the database with initial client data
+	seedClientes()
+}
+
+func seedClientes() {
+	var count int64
+	db.Model(&Cliente{}).Count(&count)
+	if count == 0 {
+		clientes := []Cliente{
+			{ID: 1, Limite: 100000, Saldo: 0},
+			{ID: 2, Limite: 80000, Saldo: 0},
+			{ID: 3, Limite: 1000000, Saldo: 0},
+			{ID: 4, Limite: 10000000, Saldo: 0},
+			{ID: 5, Limite: 500000, Saldo: 0},
+		}
+
+		for _, cliente := range clientes {
+			// Check if the Cliente already exists
+			var tempCliente Cliente
+			if db.First(&tempCliente, cliente.ID).Error == gorm.ErrRecordNotFound {
+				db.Create(&cliente) // Create only if not exists
+			}
+		}
+	}
 }
 
 func main() {
-	// Get the port number from the environment variable
+	initDatabase()
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000" // Default port if not specified
 	}
+
 	app := fiber.New()
 
 	app.Use(func(c *fiber.Ctx) error {
@@ -53,64 +105,95 @@ func main() {
 	}
 }
 
+// Handler for creating transactions
 func transacaoHandler(c *fiber.Ctx) error {
-	id, err := strconv.Atoi(c.Params("id"))
-	if err != nil || id == 6 {
-		return c.SendStatus(fiber.StatusNotFound)
-	}
+	var transacao Transacao
+	clienteID, _ := strconv.Atoi(c.Params("id"))
 
-	cliente, exists := clientes[id]
-	if !exists {
-		return c.SendStatus(fiber.StatusNotFound)
-	}
-
-	var transacao Transaction
+	// Decode request body into Transacao struct
 	if err := c.BodyParser(&transacao); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse transaction"})
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Cannot parse JSON"})
 	}
 
-	// Validate the transaction
-	if transacao.Tipo != "c" && transacao.Tipo != "d" ||
-		transacao.Descricao == "" || len(transacao.Descricao) > 10 ||
-		transacao.Valor <= 0 {
-		return c.SendStatus(fiber.StatusUnprocessableEntity)
+	// Check if descricao is empty or longer than 10 characters
+	if len(transacao.Descricao) == 0 || len(transacao.Descricao) > 10 {
+		return fiber.NewError(fiber.StatusUnprocessableEntity, "descricao deve ser uma string de 1 a 10 caracteres.")
 	}
 
-	// Apply transaction
-	novoSaldo := cliente.Saldo
-	if transacao.Tipo == "c" {
-		novoSaldo += transacao.Valor
-	} else {
-		novoSaldo -= transacao.Valor
-		if novoSaldo < -cliente.Limite {
-			return c.SendStatus(fiber.StatusUnprocessableEntity)
+	// Start a new DB transaction
+	tx := db.Begin()
+
+	// Check for the existence of the client
+	var cliente Cliente
+	result := tx.Clauses(clause.Locking{Strength: "UPDATE"}).First(&cliente, clienteID)
+	if result.Error != nil {
+		tx.Rollback()
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Cliente not found"})
+	}
+
+	// Apply transaction logic based on type (c: credit, d: debit)
+	switch transacao.Tipo {
+	case "c":
+		cliente.Saldo += transacao.Valor
+	case "d":
+		if cliente.Saldo-transacao.Valor < -cliente.Limite {
+			tx.Rollback()
+			return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{"error": "Insufficient funds"})
 		}
+		cliente.Saldo -= transacao.Valor
+	default:
+		tx.Rollback()
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{"error": "Invalid transaction type"})
 	}
 
-	cliente.Saldo = novoSaldo
-	transacao.RealizadaEm = time.Now()
-	cliente.Transacoes = append(cliente.Transacoes, transacao)
-
-	return c.JSON(fiber.Map{"limite": cliente.Limite, "saldo": cliente.Saldo})
-}
-
-func extratoHandler(c *fiber.Ctx) error {
-	id, err := strconv.Atoi(c.Params("id"))
-	if err != nil || id == 6 {
-		return c.SendStatus(fiber.StatusNotFound)
+	// Save transaction
+	transacao.ClienteID = uint(clienteID)
+	if err := tx.Create(&transacao).Error; err != nil {
+		tx.Rollback()
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to create transaction"})
 	}
 
-	cliente, exists := clientes[id]
-	if !exists {
-		return c.SendStatus(fiber.StatusNotFound)
+	// Update client saldo
+	if err := tx.Save(&cliente).Error; err != nil {
+		tx.Rollback()
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to update cliente saldo"})
+	}
+
+	// Commit the transaction
+	if err := tx.Commit().Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Transaction commit failed"})
 	}
 
 	return c.JSON(fiber.Map{
+		"limite": cliente.Limite,
+		"saldo":  cliente.Saldo,
+	})
+}
+
+// Handler for fetching account statement
+func extratoHandler(c *fiber.Ctx) error {
+	clienteID, _ := strconv.Atoi(c.Params("id"))
+
+	// Check for the existence of the client
+	var cliente Cliente
+	result := db.First(&cliente, clienteID)
+	if result.Error != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Cliente not found"})
+	}
+
+	// Load the last 10 transactions
+	var ultimasTransacoes []Transacao
+	db.Where("cliente_id = ?", clienteID).Order("created_at desc").Limit(10).Find(&ultimasTransacoes)
+
+	// Prepare the response
+	extrato := fiber.Map{
 		"saldo": fiber.Map{
 			"total":        cliente.Saldo,
 			"data_extrato": time.Now().Format(time.RFC3339),
 			"limite":       cliente.Limite,
 		},
-		"ultimas_transacoes": cliente.Transacoes,
-	})
+		"ultimas_transacoes": ultimasTransacoes,
+	}
+
+	return c.JSON(extrato)
 }
